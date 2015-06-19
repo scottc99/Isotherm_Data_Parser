f3567df80ad02096679430f21ac03602ffaab033
Ford Scott <scottc99@students.rowan.edu>
2015-06-19 14:27:11 -0400

    more changes

--- Laura_TGAData_Script/main.py
+++ Laura_TGAData_Script/main.py
@@ -1,8 +1,8 @@
 #### Script for first set of data from TGA (machine 1) ####
-import os 
-import os.path
 
 from openpyxl import load_workbook
+import os 
+import os.path
 wb = load_workbook (filename = "8852_DataSet_TGA.xlsx")
 
 print wb.get_sheet_names()
