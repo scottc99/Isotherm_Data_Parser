a421752fc80e22b922daf3e1aff129ec38f737e4
Ford Scott <scottc99@students.rowan.edu>
2015-06-24 13:03:35 -0400

    almost there with defining wb and sh for JSON transfer

--- Laura_TGAData_Script/main.py
+++ Laura_TGAData_Script/main.py
@@ -80,12 +80,13 @@ class TGApp:
 	def load_sheet(self):
 		self.sheet = self.entry.get()
 		print "Sheet No: %s"%self.sheet
-		if int(self.sheet) > 0:
-			try:
-				self.sh = self.wb.sheet_by_index(int(self.sheet) - 1)
-				self.root.destroy()
-			except:
-				print "Error loading the sheet."
+		if self.sheet == StrVar():
+			
+			sh = self.wb.sheet_by_index(int(self.sheet) - 1)
+			return sh 
+			button2 = Button(self.root, text = "Init JSON Transfer", command = self.runJSON)
+			button2.pack()
+			self.stop.mainloop()
 		else:
 			print 'Eee Roar!'
 
@@ -96,23 +97,6 @@ class TGApp:
 		self.root.mainloop()
 
 
-
-# class Student:
-# 	def __init__(self, name):
-# 		self.name = name
-
-# 	def whoAreYou(self):
-# 		print "I am %s."%self.name
-
-# 	def rename(self, newName):
-# 		self.name = newName
-
-
-
-if __name__ == '__main__':
-
-	TGInstance = TGApp()
-	TGInstance.run()
 
 	# student1 = Student("John")
 	# student2 = Student("Alice")
@@ -158,51 +142,55 @@ if __name__ == '__main__':
 #			print "Error: Wierd index!"
 
 
-# TGA_Data = OrderedDict()
+	TGA_Data = OrderedDict()
 
-# for rownum in range(1,sh.nrows):
-# 	row_values = sh.row_values(rownum)
-# 	# print row_values[9]
-# 	TGA_Data['File Name'] = row_values[0]
-# 	TGA_Data['Experiment'] = row_values[1]
-# 	TGA_Data['Operator'] = row_values[2]
-# 	TGA_Data['Experiment ID'] = row_values[3]
-# 	TGA_Data['Sample Name'] = row_values[4]
-# 	TGA_Data['Sample Lot #'] = row_values[5]
-# 	TGA_Data['Notes'] = row_values[6]
-# 	TGA_Data[''] = row_values[7]
-# 	TGA_Data['Adsorbate'] = row_values[8]
-# 	TGA_Data['Drying Temp'] = {}
-# 	TGA_Data['Drying Temp']['unit']=row_values[9][1]
-# 	TGA_Data['Drying Temp']['value']=row_values[9][0]
-# 	TGA_Data['Heating Rate'] = row_values[10]
-# 	TGA_Data['Max Drying Time'] = row_values[11]
-# 	TGA_Data['Equil Crit'] = row_values[12]
-# 	TGA_Data['Run Temp'] = row_values[13]
-# 	TGA_Data['Max Equil Time'] = row_values[14]
-# 	TGA_Data['Equil Crit'] = row_values[15]
-# 	TGA_Data['Pres Steps'] = row_values[16]
-# 	TGA_Data['Data Logging Interval'] = row_values[17]
-# 	TGA_Data['Expt Started'] = row_values[18]
-# 	TGA_Data['Run Started'] = row_values[19]
+	for rownum in range(1,sh.nrows):
+		row_values = sh.row_values(rownum)
+		# print row_values[9]
+		TGA_Data['File Name'] = row_values[0]
+		TGA_Data['Experiment'] = row_values[1]
+		TGA_Data['Operator'] = row_values[2]
+		TGA_Data['Experiment ID'] = row_values[3]
+		TGA_Data['Sample Name'] = row_values[4]
+		TGA_Data['Sample Lot #'] = row_values[5]
+		TGA_Data['Notes'] = row_values[6]
+		TGA_Data[''] = row_values[7]
+		TGA_Data['Adsorbate'] = row_values[8]
+		TGA_Data['Drying Temp'] = {}
+		TGA_Data['Drying Temp']['unit']=row_values[9][1]
+		TGA_Data['Drying Temp']['value']=row_values[9][0]
+		TGA_Data['Heating Rate'] = row_values[10]
+		TGA_Data['Max Drying Time'] = row_values[11]
+		TGA_Data['Equil Crit'] = row_values[12]
+		TGA_Data['Run Temp'] = row_values[13]
+		TGA_Data['Max Equil Time'] = row_values[14]
+		TGA_Data['Equil Crit'] = row_values[15]
+		TGA_Data['Pres Steps'] = row_values[16]
+		TGA_Data['Data Logging Interval'] = row_values[17]
+		TGA_Data['Expt Started'] = row_values[18]
+		TGA_Data['Run Started'] = row_values[19]
 
 
 
-# 	TGA_Data['Elap Time'] = {}
-# 	TGA_Data['Elap Time']['unit'] = 'min'
-# 	TGA_Data['Elap Time']['value'] = 355.7
+		TGA_Data['Elap Time'] = {}
+		TGA_Data['Elap Time']['unit'] = 'min'
+		TGA_Data['Elap Time']['value'] = 355.7
 
-# 	if rownum == 22:
-# 		TGA_Measurements = []
-	
-# 	# if rownum >= 24:
-# 	# 	TGA_raw_Data = {}
-# 	# 	for column in sh.column_values(rownum):
-# 	# 		row_values = sh.row_values(rownum) #column_values i think.
+		if rownum == 22:
+			TGA_Measurements = []
+		
+		# if rownum >= 24:
+		# 	TGA_raw_Data = {}
+		# 	for column in sh.column_values(rownum):
+		# 		row_values = sh.row_values(rownum) #column_values i think.
 
-# 	j = json.dumps(TGA_Data)
+		j = json.dumps(TGA_Data)
 
-# 	with open('data.json', 'w') as f:
-# 			f.write(j)
+		with open('data.json', 'w') as f:
+				f.write(j)
 
+if __name__ == '__main__':
+
+	TGInstance = TGApp()
+	TGInstance.run()
 
