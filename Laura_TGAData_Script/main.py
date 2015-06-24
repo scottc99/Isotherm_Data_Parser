d3f6f0aa6dc107950bbc9df20db9be7369d9afa7
Ford Scott <scottc99@students.rowan.edu>
2015-06-24 14:44:57 -0400

    Working of JSON conversion

--- Laura_TGAData_Script/main.py
+++ Laura_TGAData_Script/main.py
@@ -67,6 +67,7 @@ from Tkinter import *
 
 class TGApp:
 	def __init__(self):
+		self.sh = None
 		self.root = Tk()
 		self.label = Label(self.root, text = "Enter Sheet Number")
 		self.label.pack()
@@ -76,17 +77,18 @@ class TGApp:
 
 		button = Button(self.root, text = "Load", command = self.load_sheet)
 		button.pack()
+		button.grid_forget()
 
 	def load_sheet(self):
 		self.sheet = self.entry.get()
 		print "Sheet No: %s"%self.sheet
-		if self.sheet == StrVar():
+		if self.sheet.__class__ == type(""):
 			
-			sh = self.wb.sheet_by_index(int(self.sheet) - 1)
-			return sh 
+			self.sh = self.wb.sheet_by_index(int(self.sheet) - 1)
 			button2 = Button(self.root, text = "Init JSON Transfer", command = self.runJSON)
 			button2.pack()
-			self.stop.mainloop()
+			button2.grid_forget()
+			
 		else:
 			print 'Eee Roar!'
 
@@ -140,54 +142,57 @@ class TGApp:
 #			return 'sh = wb.sheet_by_index' + (int(int(self.e1.get()))) - 1
 #else:
 #			print "Error: Wierd index!"
+	def runJSON(self):
+	
+		TGA_Data = OrderedDict()
+
+		for rownum in range(1,self.sh.nrows):
+			TGA_Data = OrderedDict()
+			row_values = self.sh.row_values(rownum)
+			# print row_values[9]
+			TGA_Data['File Name'] = row_values[0]
+			TGA_Data['Experiment'] = row_values[1]
+			TGA_Data['Operator'] = row_values[2]
+			TGA_Data['Experiment ID'] = row_values[3]
+			TGA_Data['Sample Name'] = row_values[4]
+			TGA_Data['Sample Lot #'] = row_values[5]
+			TGA_Data['Notes'] = row_values[6]
+			TGA_Data[''] = row_values[7]
+			TGA_Data['Adsorbate'] = row_values[8]
+			TGA_Data['Drying Temp'] = row_values[9]
+			# TGA_Data['Drying Temp']['unit']=row_values[9][1]
+			# TGA_Data['Drying Temp']['value']=row_values[9][0]
+			TGA_Data['Heating Rate'] = row_values[10]
+			TGA_Data['Max Drying Time'] = row_values[11]
+			TGA_Data['Equil Crit'] = row_values[12]
+			TGA_Data['Run Temp'] = row_values[13]
+			TGA_Data['Max Equil Time'] = row_values[14]
+			TGA_Data['Equil Crit'] = row_values[15]
+			TGA_Data['Pres Steps'] = row_values[16]
+			TGA_Data['Data Logging Interval'] = row_values[17]
+			TGA_Data['Expt Started'] = row_values[18]
+			TGA_Data['Run Started'] = row_values[19]
 
 
-	TGA_Data = OrderedDict()
 
-	for rownum in range(1,sh.nrows):
-		row_values = sh.row_values(rownum)
-		# print row_values[9]
-		TGA_Data['File Name'] = row_values[0]
-		TGA_Data['Experiment'] = row_values[1]
-		TGA_Data['Operator'] = row_values[2]
-		TGA_Data['Experiment ID'] = row_values[3]
-		TGA_Data['Sample Name'] = row_values[4]
-		TGA_Data['Sample Lot #'] = row_values[5]
-		TGA_Data['Notes'] = row_values[6]
-		TGA_Data[''] = row_values[7]
-		TGA_Data['Adsorbate'] = row_values[8]
-		TGA_Data['Drying Temp'] = {}
-		TGA_Data['Drying Temp']['unit']=row_values[9][1]
-		TGA_Data['Drying Temp']['value']=row_values[9][0]
-		TGA_Data['Heating Rate'] = row_values[10]
-		TGA_Data['Max Drying Time'] = row_values[11]
-		TGA_Data['Equil Crit'] = row_values[12]
-		TGA_Data['Run Temp'] = row_values[13]
-		TGA_Data['Max Equil Time'] = row_values[14]
-		TGA_Data['Equil Crit'] = row_values[15]
-		TGA_Data['Pres Steps'] = row_values[16]
-		TGA_Data['Data Logging Interval'] = row_values[17]
-		TGA_Data['Expt Started'] = row_values[18]
-		TGA_Data['Run Started'] = row_values[19]
+			# TGA_Data['Elap Time'] = {}
+			# TGA_Data['Elap Time']['unit'] = 'min'
+			# TGA_Data['Elap Time']['value'] = 355.7
 
+			# if rownum == 22:
+			# 	TGA_Measurements = []
+			
+			# if rownum >= 24:
+			# 	TGA_raw_Data = {}
+			# 	for column in sh.column_values(rownum):
+			# 		row_values = sh.row_values(rownum) #column_values i think.
 
+			j = json.dumps(TGA_Data)
 
-		TGA_Data['Elap Time'] = {}
-		TGA_Data['Elap Time']['unit'] = 'min'
-		TGA_Data['Elap Time']['value'] = 355.7
+			with open('data.json', 'w') as f:
+					f.write(j)
 
-		if rownum == 22:
-			TGA_Measurements = []
-		
-		# if rownum >= 24:
-		# 	TGA_raw_Data = {}
-		# 	for column in sh.column_values(rownum):
-		# 		row_values = sh.row_values(rownum) #column_values i think.
-
-		j = json.dumps(TGA_Data)
-
-		with open('data.json', 'w') as f:
-				f.write(j)
+			self.root.destroy()
 
 if __name__ == '__main__':
 
