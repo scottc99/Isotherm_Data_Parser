53115feaa6e6b80e356b4e092d9d421fbf44228d
Ford Scott <scottc99@students.rowan.edu>
2015-07-09 14:32:54 -0400

    First draft of setup.py/__init__.py files

--- setup.py
+++ setup.py
@@ -5,17 +5,22 @@ from codecs import open
 import os
 
 setup(
-    name = "Isotherm Data Parser",
+    name = "Isotherm_Data_Parser",
     version = "0.1.0.dev1",
     packages = ['setuptools', 'setuptools.setup', 'codecs', 'codecs.open', 'collections', 'collections.OrderedDict',
                 'Tkinter', 'Tkinter.*', 'matplotlib.pyplot', 'matplotlib.pyplot.show', 'matplotlib.pyplot.plot', 
                 'matplotlib.pyplot.ion', 'json', 'lxml']
-    py_modules = ['glob', 'xlrd', 'xlwt', 'simplejson', 'matplotlib', 'pylab', 'numpy', 'dicttoxml', 'lxml']
+    py_modules = ['glob', 'xlrd', 'xlwt', 'simplejson', 'matplotlib', 'pylab', 'numpy', 'dicttoxml', 'lxml', 'time']
     url = "https://github.com/scottc99/Isotherm_Data_Parser"
     author = "NIST: Group 642, 643 Contributers",
     author_email = "scottc99@students.rowan.edu",
     description = "A tool designed to parse through isotherm data and convert data into json and xml formats and plot. ",
     long_description = open('README.rst').read(),
     install_requires = ['libxml2 >= 2.9.0', 'libxslt >= 1.1.26']
+    entry_points={
+        'console_scripts': [
+            'Isotherm_Data_Parser = Isotherm_Data_Parser.run:main'
+        ]
+    }        
 )
 
