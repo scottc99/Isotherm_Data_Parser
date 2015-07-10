692ae767cd10f565fbdba1d38d464099e288b71e
Ford Scott <scottc99@students.rowan.edu>
2015-07-10 02:30:01 -0400

    working on icon still

--- setup.py
+++ setup.py
@@ -12,11 +12,9 @@ setup(
     author = "NIST: Group 642, 643 Contributers",
     author_email = "scottc99@students.rowan.edu",
     description = "A tool designed to parse through isotherm data and convert data into json and xml formats and plot.",
-    install_requires = ['setuptools >= 17.0', 'matplotlib >= 1.4.3', 'numpy >= 1.9.2', 
+    install_requires = ['matplotlib >= 1.4.3', 'numpy >= 1.9.2', 
                         'simplejson >= 3.7.3', 'xlrd >= 0.9.3', 'xlwt >= 0.7.5', 
                         'pprint >= 0.2.3', 'codecs >= 7.8', 'collections', 
                         'dicttoxml >= 1.6.6', 'lxml >= 3.4.2', 'libxml2 >= 2.9.0', 
                         'libxslt >= 1.1.26', 'py2exe >= 0.9.2.2'],
-    console = ['run.py'],
-    windows = ['GUI.py']
 )
